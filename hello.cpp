36c9b79a933bd190c5d01ad03423a34022fd10d7
Guy Coen <guy.coen@icloud.com>
2026-08-12 14:00:28 +0800

    print version

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
 
+#ifndef APP_VERSION
+#define APP_VERSION "0.0.0"
+#endif
+
 std::string greeting(const std::string& who) {
     return "Hello, " + who + "!";
 }
 
 int main() {
     std::cout << greeting("World") << "\n";
+    std::cout << "version " << APP_VERSION << "\n";
     return 0;
 }
