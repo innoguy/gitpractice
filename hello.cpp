5c179e4928e62736a447f6edc4ee3f60710b7700
Guy Coen <guy.coen@icloud.com>
2026-08-12 14:49:00 +0800

    add farewell message (#2)

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,12 @@ std::string greeting(const std::string& who) {
     return "Hello, " + who + "!";
 }
 
+std::string farewell(const std::string& who) {
+    return "Goodbye, " + who + "!";
+}
+
 int main() {
     std::cout << greeting("World") << "\n";
+    std::cout << farewell("World") << "\n";
     return 0;
 }
